472c4bc11fc87f7b72648be4a835d2579ada9c9b
paforsey <Philip.Forsey@Outlook.com>
2015-06-09 13:38:00 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,67 +1,30 @@
-library(shiny);
-library(ggplot2);
-library(dichromat);
-
-
-shinyUI(fluidPage(
-    fluidRow(div(style="padding-left:20px;",
-        h1("Audit Risk Score")
-    )),
-
-    fluidRow(
-        column(3, div(style="background-color:#87CEFA; border: solid 1px lightgray; 
-            border-radius:10px; padding:20px; margin-bottom:15px; margin-top:0px;",
-            dateInput("FundedDate", "Funded Date:"),
-            numericInput(inputId="LoanAmt",label="Loan Amount:", value=20000, min=0, 
-                max=100000, step=250),
-            numericInput(inputId="Wholesale",label="Wholesale Value:", value=16000, 
-                min=0, max=100000, step=250),
-            checkboxGroupInput("Profile", "Vehicle Profile:",
-                c(
-                    "1) Used"=1, 
-                    "2) Vehicle Age > 5"=2,
-                    "3) Mileage > 100,000"=3)
-                )),
-            submitButton('Submit')
-        ),
-        column(3,div(style="background-color:white; border: solid 1px lightgray; 
-            border-radius:10px; padding:5px 15px 5px 15px;",
-            h4('Funded Date:'),
-                verbatimTextOutput("odate"),
-            h4('Loan Amount:'),
-                verbatimTextOutput("oid1"),
-            h4('Wholesale Value:'),
-                verbatimTextOutput("oid2"),
-            h4('Vehicle Profile:'),
-                verbatimTextOutput("oid3"),
-            h4('Audit Risk Score:'),
-                verbatimTextOutput("RiskScore")
-        ))
-    ),
-
+RiskScore <- function(LoanAmt, Wholesale, Profile){
+    ScoreMin = 500
+    ScoreMax = 900 - (3 * 25)
+    LTVMin = 0
+    LTVMax = 2.00
     
-    fluidRow(div(style="padding-left:15px; padding-top: 15px;",
-        h4('Documentation'),
-        h5('The purpose of this model is to calculate a score ranging from 500-900 
-            that assesses a loan applicant\'s risk; higher scores indicate increased 
-            levels of risk.  The score takes into account the loan amount and 
-            wholesale value of the vehicle as well as its general condition.'),
-        h5('-'),
-        h5('Notes:'),
-        h5('1) Model inputs are located in the light blue shaded area above on 
-            the left.'),
-        h5('2) Model outputs are located in the unshaded are above on the right.'),
-        h5('-'),
-        h5('Instructions:'),
-        h5('1) Enter the date the loan was funded.'),
-        h5('2) Enter the amount of the loan.'),
-        h5('3) Enter the wholesale amount of the vehicle.'),
-        h5('4) Select all of the applicable vehicle profile attributes.'),
-        h5('5) Select the "Submit" button.'),
-        h5('6) The score will update at the bottom of the output area under the 
-            heading "Audit Risk Score."')
-        )
-    ))
+    LTV = LoanAmt / Wholesale
+    Penalty = 25 * length(Profile)
+    
+    RiskScore = ((ScoreMax - ScoreMin) / (LTVMax - LTVMin) * LTV) + (ScoreMax - 
+        (LTVMax * ((ScoreMax - ScoreMin) / (LTVMax - LTVMin))))
+    RiskScore =  round(RiskScore + Penalty, 0)
+    if(RiskScore > 900){
+        RiskScore = 900
+    }
+    RiskScore
+    
+};
 
-)
+shinyServer(
+    function(input, output){
+        output$oid1 <- renderPrint({input$LoanAmt})
+        output$oid2 <- renderPrint({input$Wholesale})
+        output$oid3 <- renderPrint({input$Profile})
+        output$odate <- renderPrint({input$FundedDate})
+        output$RiskScore <- renderPrint({RiskScore(input$LoanAmt, 
+            input$Wholesale, input$Profile)})
+    }
+);
 
